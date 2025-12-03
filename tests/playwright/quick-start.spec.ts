82a52a28cab7364466ac55c90c14af1b6cc83469
Xudong Guo <guoxudong.dev@gmail.com>
2025-12-03 11:04:22 +0800

    fix

--- tests/playwright/quick-start.spec.ts
+++ tests/playwright/quick-start.spec.ts
@@ -21,23 +21,23 @@ test.describe('Quick Start', () => {
 
     // 3) 点击“启动容器”，等待状态从“启动中”到“运行中”
     await page.getByRole('button', { name: '启动容器' }).click();
-    await expect(page.getByText('运行中')).toBeVisible({ timeout: 120_000 });
-    await expect(page.getByText(':/kaiwudb/')).toBeVisible({ timeout: 120_000 });
+    await expect(page.getByText('运行中')).toBeVisible({ timeout: 120000 });
+    await expect(page.getByText(':/kaiwudb/')).toBeVisible({ timeout: 120000 });
 
     // 4) 点击“下一步”，确认进入下一步
     await page.getByRole('button', { name: '下一步' }).click();
     await page.getByRole('paragraph').filter({ hasText: '切换至程序目录： cd /kaiwudb/binRun' }).getByRole('button').click();
-    await expect(page.locator('span').filter({ hasText: 'cd /kaiwudb/bin' })).toBeVisible({ timeout: 120_000 });
+    await expect(page.locator('span').filter({ hasText: 'cd /kaiwudb/bin' })).toBeVisible({ timeout: 120000 });
 
     // 5) 点击 Run
     await page.getByRole('paragraph').filter({ hasText: '启动 KWDB（非安全模式）：./kwbase start' }).getByRole('button').click();
-    await expect(page.getByText('increase the replication')).toBeVisible({ timeout: 120_000 });
+    await expect(page.getByText('increase the replication')).toBeVisible({ timeout: 120000 });
     await page.getByRole('paragraph').filter({ hasText: '使用 kwbase sql 连接到数据库：./kwbase' }).getByRole('button').click();
-    await expect(page.getByText('root@127.0.0.1:26257/')).toBeVisible({ timeout: 120_000 });
+    await expect(page.getByText('root@127.0.0.1:26257/')).toBeVisible({ timeout: 120000 });
 
     // 6) 点击“停止容器”
     await page.getByRole('button', { name: '停止容器' }).click();
-    await expect(page.getByText('停止中')).toBeVisible({ timeout: 120_000 });
-    await expect(page.getByText('请先启动容器')).toBeVisible({ timeout: 240_000 });
+    // await expect(page.getByText('停止中')).toBeVisible({ timeout: 120000 });
+    await expect(page.getByText('请先启动容器')).toBeVisible({ timeout: 240000 });
   });
 });
